8f3797b2b2901676fb73a211ded01a0e8d5c7f31
yixianghsy <yixianghsy@gmail.com>
2020-04-09 19:09:37 +0800

    initg

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,58 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    component: () => import('@/components/CommonLayout.vue'),
+    children:[
+      {
+        path:'',
+        component:() => import('@/views/Home.vue'),
+        name:'home'
+      },
+      {
+        path:'/detail/:id',
+        component:() => import('@/views/Detail.vue')
+      },
+      {
+        path:'/personal',
+        component:() => import('@/views/Personal.vue'),
+        meta:{
+          requireAuth:true//true为这个页面需要登录权限
+        }
+      },
+      {
+        path:'/article',
+        component:() => import('@/views/Article.vue'),
+        meta:{
+          requireAuth:true//true为这个页面需要登录权限
+        },
+        name:'article'
+      },
+      {
+        path:'/article/edit',
+        name:'editArticle',
+        component:() => import('@/views/ArticleEdit.vue'),
+        meta:{
+          requireAuth:true//true为这个页面需要登录权限
+        }
+      },
+      {
+        path:'/article/edit/:id',
+        name:'updateArticle',
+        component:() => import('@/views/ArticleEdit.vue'),
+        meta:{
+          requireAuth:true//true为这个页面需要登录权限
+        }
+      }
+    ]
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path:'/login',
+    component:() => import('@/views/Login.vue')
   }
 ];
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,16 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    isSignIn: 0,
+    token:''
+  },
+  mutations: {
+    changIsSignIn (state,n) {
+      state.isSignIn = n
+    },
+    setToken(state,val) {
+      state.token = val
+    }
+  },
 });
